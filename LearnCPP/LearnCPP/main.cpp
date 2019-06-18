134eab0a056ee357549ac289bbcd68deba7446fe
Andrey <user@mail.ru>
2019-06-19 01:11:54 +0700

    Вычисление Value x 2

--- LearnCPP/LearnCPP/main.cpp
+++ LearnCPP/LearnCPP/main.cpp
@@ -1,7 +1,14 @@
 
+#include <iostream>
+
 int  main(int argc, char* args[])
 {
-	// comments
+	unsigned int value = 0;
+
+	std::cout << "Input value: ";
+	std::cin >> value;
+
+	std::cout << "Value x 2 = " << value * 2 << std::endl;
 
 	return 0;
 }
